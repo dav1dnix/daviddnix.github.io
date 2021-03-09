82605add984685f3c66ef337f78d75a9440a5093
David Smith <dps141618@gmail.com>
2021-03-09 08:58:57 +0000

    hh

--- js/rainbow.js
+++ js/rainbow.js
@@ -13,8 +13,7 @@ const violet = "./assets/icons/rainbow/violet.png";
 // Every second, change icon in order
 let current = 0;
 
-// Add null as first element, forcing the array to start from red instead of orange.
-const icons = [null, red, orange, yellow, green, blue, indigo, violet];
+const icons = [red, orange, yellow, green, blue, indigo, violet];
 
 window.setInterval(function() {
     // What this does is it adds to the current, then uses % to get the remainder which is 0. It will go along until it gets to the last element index, and then restart. 

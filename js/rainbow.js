6ea353d640a6df909bb4a8adcb8dd4e5f9c69e52
David Smith <dps141618@gmail.com>
2021-03-09 08:57:27 +0000

    force start at red instead of orange, add comment so I don't forget what it does

--- js/rainbow.js
+++ js/rainbow.js
@@ -12,10 +12,13 @@ const violet = "./assets/icons/rainbow/violet.png";
 
 // Every second, change icon in order
 let current = 0;
-const icons = [red, orange, yellow, green, blue, indigo, violet];
-window.setInterval(function() {
-    const i = (++current % icons.length);
-    const url = icons[i];
 
-    icon.href = url
+// Add null as first element, forcing the array to start from red instead of orange.
+const icons = [null, red, orange, yellow, green, blue, indigo, violet];
+
+window.setInterval(function() {
+    // What this does is it adds to the current, then uses % to get the remainder which is 0. It will go along until it gets to the last element index, and then restart. 
+    const url = icons[++current % icons.length];
+
+    icon.href = url;
 }, 1000);

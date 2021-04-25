2abed4b78a6eff74828d1b959e7f4d6e19beb15d
David Smith <dps141618@gmail.com>
2021-04-25 18:28:33 +0100

    added github info to html

--- js/github.js
+++ js/github.js
@@ -1,17 +1,32 @@
-const url = "https://api.github.com/users/dps910"
+const url = "https://api.github.com/users/dps910";
 const options = {
 	headers: {
 		"User-Agent": "dps910"
 	},
 };
 
-const request = fetch(url, options)
+const request = fetch(url, options);
 
 // Fulfil request
 request.then((data) => {
 	if (data.status == 200 || data.status == 301) {
-		return data.json()
+		data.json().then((data) => {
+
+			const id = document.getElementById("git")
+
+			id.setAttribute("style", "white-space: pre-line;");
+
+			id.textContent = `
+				username: ${data.login}
+				bio: ${data.bio}
+				repos: ${data.public_repos}
+				gists: ${data.public_gists}
+				location: ${data.location}
+			`
+			console.log(data)
+		});
+		// document.getElementById("git").innerHTML = data.json()
 	} else {
-		throw new Error("Couldn't reach GitHub API")
+		throw new Error("Couldn't reach GitHub API");
 	}
 });
